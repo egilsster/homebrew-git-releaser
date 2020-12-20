3f29446c0efb8752398a8ae5a1c499303dbb572a
Egill Sveinbjörnsson <egillsveinbjorns@gmail.com>
2020-12-20 16:35:58 +0100

    chore: release v0.2.0

--- Formula/git-releaser.rb
+++ Formula/git-releaser.rb
@@ -2,8 +2,8 @@ class GitReleaser < Formula
     desc "Creates a git tag, a changelog and a git release, all in one command."
     homepage "https://github.com/egilsster/git-releaser"
     url "https://github.com/egilsster/git-releaser/releases/latest/download/git-releaser-x86_64-apple-darwin.tar.gz"
-    sha256 "f46fe7f72c7f5482953b46a3c3ce6e37478238925374e573cbff3b09b16f232c"
-    version "0.1.0"
+    sha256 "5949a40c60d5dc04a56ce7fe5fffd9052e2dbc2137cf8575af431f2fac882806"
+    version "0.2.0"
 
     def install
       bin.install "git-releaser"
